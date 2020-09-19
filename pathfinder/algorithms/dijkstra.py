c4aa077a3105db998168b317a1d43d6eee60ed6b
Ryan <rpfarish@live.com>
2020-09-19 11:12:26 -0400

    Refactored to have a priority queue

--- pathfinder/algorithms/dijkstra.py
+++ pathfinder/algorithms/dijkstra.py
@@ -1,45 +1,39 @@
 from pathfinder.search.adjacent_nodes import adjacent_nodes
-
-
-def min_of_grid_in_check_node(grid, check_node):
-    seq = {i: grid[i] for i in check_node}
-    node = float("inf")
-    a, b = None, None
-    for key, val in seq.items():
-        if val < node:
-            node = val
-            a = key
-            b = val
-    return a, b
+from queue import PriorityQueue
 
 
 def dijkstra(start, end, wall, grid_size,  weight_li):
-    """TODO put check node in a PriorityQueue"""
+    """
+    The famous Dijkstra's Algorithm invented by Edsger Dijkstra and
+    implemented with a priority queue.
+    """
     grid = {}
-    check_node = []
     prev = {start: None}
+    queue = PriorityQueue()
+
     for x in range(grid_size[0] + 1):
         for y in range(grid_size[1] + 1):
             if (x, y) not in wall:
-                check_node.append((x, y))
-            grid[(x, y)] = float("inf")
+                grid[(x, y)] = float("inf")
 
+    queue.put((0, start))
     grid[start] = 0
-    visit_order = []
-    while check_node:
-        curr, curr_dist = min_of_grid_in_check_node(grid, check_node)
+    visited = []
+    while not queue.empty():
+        curr_dist, curr = queue.get()
+
         if curr is None or curr == end:
             break
 
-        check_node.remove(curr)
         for adj in adjacent_nodes(curr, wall, grid_size):
             pot_dist = curr_dist + weight_li[adj]
             if pot_dist < grid[adj]:
-                if adj not in visit_order:
-                    visit_order.append(adj)
+                if adj not in visited:
+                    visited.append(adj)
+                    queue.put((pot_dist, adj))
                 grid[adj] = pot_dist
                 prev[adj] = curr
-    return grid, prev, visit_order
+    return grid, prev, visited
 
 
 if __name__ == "__main__":
