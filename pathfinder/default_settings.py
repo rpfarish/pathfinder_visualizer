f6d3b0e46307b85bf563e0c50d57ea320564d935
Ryan <rpfarish@live.com>
2021-08-26 18:02:46 -0400

    update default_settings.py

--- pathfinder/default_settings.py
+++ pathfinder/default_settings.py
@@ -1,21 +1,23 @@
+"""Default settings backup in the case of settings.json getting deleted"""
+
 default_settings_backup = {
     "file": "settings.json",
     "version": "3.1.4",
+    "default_alg": "dijkstra",
     "dark_mode": True,
+    "screen_size": {
+        "WIDTH": 1355,
+        "HEIGHT": 680
+    },
     "grid_size": {
         "WIDTH": 50,
         "HEIGHT": 25
     },
     "node_size": 25,
-    "default_alg": "dijkstra",
-    "screen_size": {
-        "WIDTH": 1355,
-        "HEIGHT": 680
-    },
-    "search_speed": 0.0055,
     "path_speed": 0.07,
+    "search_speed": 0.0055,
     "weight_density": 10,
-    "maze_density": 0.35,
+    "maze_density_percentage": 0.33,
     "enable_diagonals": False,
     "visualize_when_dragging": True
 }
