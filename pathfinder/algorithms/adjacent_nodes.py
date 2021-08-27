8b632876cfe0ae5adf9df605ca589665785bf2a5
Ryan <rpfarish@live.com>
2021-08-27 13:09:36 -0400

    refactoring and cleaning up code

--- pathfinder/algorithms/adjacent_nodes.py
+++ pathfinder/algorithms/adjacent_nodes.py
@@ -30,7 +30,6 @@ def adjacent_nodes(node: tuple, wall, grid_size):
     return adj
 
 
-# @timer
 def adjacent_diagonals(node, wall, grid_size):
     """
     Supports diagonals using rotation via matrix multiplication
@@ -40,13 +39,13 @@ def adjacent_diagonals(node, wall, grid_size):
     """
     adjacent = {}
     rotation = np.array([[0, 1], [-1, 0]])  # clockwise 90-degrees
-    # calculate the adj node in the form:
-    # adjacent = Node + AY (where A is the rotation, Y is the initial direction and all three are matrices)
+    # Calculate the adj node in the form:
+    # adjacent := Node + AY (where A is the rotation, Y is the initial direction and all three are matrices)
 
     direction = np.array([1, 1])  # diagonal up-right
     for angle in range(4):
         x, y = potential_node = tuple(np.add(node, np.matmul(direction, matrix_power(rotation, angle))))
         if potential_node not in wall and 0 <= x <= grid_size[0] and 0 <= y <= grid_size[1]:
-            adjacent[potential_node] = 2 ** 0.5
+            adjacent[potential_node] = np.sqrt(2)
 
     return adjacent
--- pathfinder/utils.py
+++ pathfinder/utils.py
@@ -8,53 +8,57 @@ from .constants import GRID_X, GRID_Y, OFFSET
 
 
 def logger(fn):
-    """decorator name to log method calls from classes"""
+    """Decorator to log method calls from classes"""
+
     @functools.wraps(fn)
     def func(class_obj, *args, **kwargs):
-        """logs method calls from classes"""
+        """Logs method calls from classes"""
         print(f'{class_obj.__class__.__name__}.{fn.__name__} was run with "{args}" args and {kwargs} kwargs')
         return fn(class_obj, *args, **kwargs)
 
     return func
 
 
-times = []
-
-
 def timer(fn):
-    """decorator name to log method calls from classes"""
+    """Decorator to time function calls"""
+    times = []
 
     @functools.wraps(fn)
     def func(class_obj, *args, **kwargs):
-        """logs method calls from classes"""
+        """Calculates the single and average time taken per call"""
         start = time.perf_counter()
         result = fn(class_obj, *args, **kwargs)
         end = time.perf_counter()
+
         print(f'{fn.__name__} was run in {round(end - start, 6)} seconds')
         times.append(round(end - start, 6))
         print('Average:', average(times))
+
         return result
 
     return func
 
 
 def cache(fn):
-    """decorator to cache inputs to functions"""
+    """Decorator to cache inputs to functions"""
     _cache = []
     _results = []
 
     @functools.wraps(fn)
     def func(*args):
-        """cache stuff"""
+        """
+        :returns result of function if input was cached,
+        else it calls fn and returns the result
+        """
         if len(_cache) > 1000:
             _cache.clear()
             _results.clear()
-        if args not in _cache:
-            result = fn(*args)
-            _cache.append(args)
-            _results.append(result)
-        else:
+        if args in _cache:
             return _results[_cache.index(args)]
+
+        result = fn(*args)
+        _cache.append(args)
+        _results.append(result)
 
         return result
 
@@ -62,7 +66,7 @@ def cache(fn):
 
 
 def constrain(val, min_val, max_val) -> int:
-    """keeps val in the range of min_val and max_val"""
+    """Keeps val in the range of min_val and max_val"""
     return min(max_val, max(min_val, val))
 
 
@@ -81,3 +85,11 @@ def get_node_pos(node, mouse) -> tuple[int, int]:
     cur_node_x = remap(mouse[0], OFFSET, node.grid[xgr].x + node.grid[xgr].width, 0, GRID_X)
     cur_node_y = remap(mouse[1], OFFSET, node.grid[ygr].y + node.grid[ygr].height, 0, GRID_Y)
     return cur_node_x, cur_node_y
+
+
+def key_lock(keys, index: int, prev_states: list):
+    """:index key pressed :returns if the key was pressed"""
+    key_state, prev_state = keys[index], prev_states[index]
+    is_on_press = key_state and not prev_state  # Detects logic rising edge
+    prev_states[index] = key_state
+    return is_on_press
