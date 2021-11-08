4f19a217f4c50000a959c7cfc05c0a9468fff578
rpfar <rpfarish@live.com>
2021-11-08 18:14:20 -0500

    update default_settings.py

--- pathfinder/default_settings.py
+++ pathfinder/default_settings.py
@@ -3,7 +3,7 @@
 default_settings_backup = {
     "file": "settings.json",
     "version": "3.1.4",
-    "default_alg": "dijkstra",
+    "default_alg": "astar",
     "dark_mode": True,
     "screen_size": {
         "WIDTH": 1355,
@@ -15,7 +15,7 @@ default_settings_backup = {
     },
     "node_size": 25,
     "path_speed": 0.07,
-    "search_speed": 0.0055,
+    "search_speed": 0.002,
     "weight_density": 10,
     "maze_density_percentage": 0.33,
     "enable_diagonals": False,
